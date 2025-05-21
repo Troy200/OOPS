de6be6e2725999f7c943ba9d3386dd5b0872aa7c
Troy200 <kuriyanthomas27@gmail.com>
2025-05-21 18:01:37 +0200

    Add files via upload

---
+++ Lessononeintro.py
@@ -0,0 +1,12 @@
+class students():
+
+    def __init__(self,name,age):
+        self.name= name
+        self.age= age
+
+
+s1= students("Kuriyan",13)
+s2= students("Quinn",12)
+
+print(s1.name)
+print(s2.age)
